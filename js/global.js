3eb34aef77d9009261f725bbc026916161781cda
Ilia Kulyshkin <dikartone@gmail.com>
2018-12-27 17:59:11 +0300

    fix i18n loading

--- js/global.js
+++ js/global.js
@@ -1,6 +1,7 @@
-jQuery(function($) {
-    $.i18n().load( {
-        'en': './i18n/en.json',
-        'ru': './i18n/ru.json'
-    } );
+$.i18n().load({
+    'en': '/i18n/en.json',
+    'ru': '/i18n/ru.json'
+});
+$(function () {
+    $('html').i18n();
 });
